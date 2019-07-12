6338a1b0890a320a176c93e63251067eb82fa37d
Christina Landvytovych <Crofty.land@gmail.com>
2019-07-12 09:38:41 +0300

    input component and conection with server

--- public/joinWs.js
+++ public/joinWs.js
@@ -3,9 +3,7 @@ function joinWs(endpoint){
         // check to see if wsSocket is actually a socket
         wsSocket.close();
         // remove the eventListener before it's added again
-        // document.querySelector('#user-input').removeEventListener('submit',formSubmission);
-        document.querySelector('#hero--button-2').removeEventListener('submit',confirmSuggest)
-        document.querySelector('#hero--button-1').removeEventListener('submit',coffeeMaker);
+        document.querySelector('#user-input').removeEventListener('submit',formSubmission);
 
     }
     wsSocket = io(`http://localhost:9000${endpoint}`)
@@ -37,51 +35,25 @@ function joinWs(endpoint){
         joinRoom(topRoomName)
         
     })
-    // wsSocket.on('messageToClients',(msg)=>{
-    //     console.log(msg)
-    //     const newMsg = buildHTML(msg);
-    //     document.querySelector('#messages').innerHTML += newMsg
-    // })
-    // document.querySelector('.message-form').addEventListener('submit',formSubmission);
-
-
-
-    wsSocket.on('coffee', (phrase)=>{
-        console.log(phrase)
-        const newWish = wishCoffee(phrase);
-        document.querySelector('#messages').innerHTML += newWish
+    wsSocket.on('messageToClients',(msg)=>{
+        console.log(msg)
+        const newMsg = buildHTML(msg);
+        document.querySelector('#messages').innerHTML += newMsg
     })
-    document.querySelector('.message-form').addEventListener('submit',coffeeMaker);
+    document.querySelector('.message-form').addEventListener('submit',formSubmission);
 
-    wsSocket.on('confirm', (con)=>{
-        console.log(con)
-        const confirmation = confirm(con);
-        document.querySelector('#messages').innerHTML += confirmation
-    })
-    document.querySelector('.message-form').addEventListener('submit',confirmSuggest)
+
+
 
 }
 
-// function formSubmission(event){
-//     event.preventDefault();
-//     const newMessage = document.querySelector('#user-message').value;
-//     console.log(newMessage);
-//     wsSocket.emit('newMessageToServer',{text: newMessage})
-// }
-
-function confirmSuggest(e){
-    console.log(e);
-    e.preventDefault();
-    const newConfirm = document.querySelector('#user-confirm').value;
-    wsSocket.emit('confirmToServer',{text: newConfirm})
+function formSubmission(event){
+    event.preventDefault();
+    const newMessage = document.querySelector('#user-message').value;
+    console.log(newMessage);
+    wsSocket.emit('newMessageToServer',{text: newMessage})
 }
 
-function coffeeMaker(evn){
-    evn.preventDefault();
-    console.log(evn);
-    const newCoffee = document.querySelector('#coffee-message').value;
-    wsSocket.emit('wishCoffeeToServer',{text: newCoffee})
-}
 
 function buildHTML(msg){
     const convertedDate = new Date(msg.time).toLocaleString();
@@ -99,35 +71,3 @@ function buildHTML(msg){
     return newHTML;
 }
 
-function wishCoffee(phase){
-    const convertedDate = new Date(phase.time).toLocaleString();
-    const coffeeForm = `
-    <li>
-        <div class="user-image">
-            <img src="${phase.avatar}" />
-        </div>
-        <div class="user-message">
-            <div class="user-name-time">${phase.username} <span>${convertedDate}</span></div>
-            <div class="message-text">I want coffee.Who with me?</div>
-        </div>
-    </li>    
-    `
-    return coffeeForm;
-}
-
-function confirm(con){
-    const convertedDate = new Date(con.time).toLocaleString();
-    const confirmationForm = `
-    <li>
-        <div class="user-image">
-            <img src="${con.avatar}" />
-        </div>
-        <div class="user-message">
-            <div class="user-name-time">${con.username} <span>${convertedDate}</span></div>
-            <div class="message-text">I wish</div>
-            <div class="message-text">Congrats! What a match with ${con.username}! Run away and make dreams come true</div>
-        </div>
-    </li>    
-    `
-    return confirmationForm;
-}
